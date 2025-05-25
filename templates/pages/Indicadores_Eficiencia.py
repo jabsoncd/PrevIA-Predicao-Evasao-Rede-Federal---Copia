4c0db0a578248d615f5eab7188a75b7662483ea5
jabsoncd <jabsoncd@gmail.com>
2025-05-25 17:18:01 -0300

    tsety

--- templates/pages/Indicadores_Eficiencia.py
+++ templates/pages/Indicadores_Eficiencia.py
@@ -46,9 +46,9 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.markdown(
     """
     <style>
-    section[data-testid="stSidebar"] a[href$="templates/Home_Eficiencia"],
-    section[data-testid="stSidebar"] a[href$="pages/Indicadores_Eficiencia"],
-    section[data-testid="stSidebar"] a[href$="pages/Simulador_Eficiencia"] {
+    section[data-testid="stSidebar"] a[href*="templates/Home_Eficiencia"],
+    section[data-testid="stSidebar"] a[href*="pages/Indicadores_Eficiencia"],
+    section[data-testid="stSidebar"] a[href*="pages/Simulador_Eficiencia"] {
         display: none !important;
     }
     </style>
