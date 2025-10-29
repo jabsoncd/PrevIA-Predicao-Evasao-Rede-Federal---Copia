0cb15e422753c294a89703236c928bc8c494a910
jabsoncd <jabsoncd@gmail.com>
2025-10-29 13:02:18 -0300

    col_centtal

--- templates/pages/Simulador_Eficiencia_Layout.py
+++ templates/pages/Simulador_Eficiencia_Layout.py
@@ -677,18 +677,14 @@ if submit:
         st.write(f"🔴 Probabilidade de **EVADIR**: {prob_evasao:.2%}")
         
         # Exibir a imagem correspondente
-        # st.image(imagem, caption=legenda)  # use_container_width=True
+        st.image(imagem, caption=legenda, width=350)  # use_container_width=True
 
-        # Centralizar e redimensionar a imagem (400px)
-        st.markdown(
-            f"""
-            <div style='display: flex; justify-content: center;'>
-                <img src='{imagem}' alt='{legenda}' width='400'>
-            </div>
-            <p style='text-align:center; font-style: italic;'>{legenda}</p>
-            """,
-            unsafe_allow_html=True
-        )       
+        # Exibir a imagem correspondente (centralizada e com largura 400px)
+        # col1, col2, col3 = st.columns([1, 2, 1])   # a coluna do meio é mais larga
+        # with col2:
+        #     st.image(imagem, caption=legenda, width=400)
+
+        
 
 
 
