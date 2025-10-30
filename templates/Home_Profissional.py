57281addde8dedb75ca81c370902538979fa3d25
jabsoncd <jabsoncd@gmail.com>
2025-10-30 18:24:06 -0300

    multiv2

--- templates/Home_Profissional.py
+++ templates/Home_Profissional.py
@@ -166,7 +166,7 @@ st.markdown(f"""
     <div class="nav-logo"></div>
     <div class="nav-links">
         <span class="nav-link" onclick="window.location.href='/';">Início</span>
-        <span class="nav-link" onclick="window.location.href='/Simulador_Eficiencia_Layout';">Simular</span>
+        <span class="nav-link" onclick="window.location.href="Simulador_Eficiencia_Layout";">Simular</span>
         <span class="nav-link" onclick="window.location.href='/Indicadores_Eficiencia_Layout';">Indicadores</span>
         <span class="nav-link" onclick="window.location.href='#gestor';">Módulo Gestor</span>
         <span class="nav-link" onclick="window.location.href='#sobre';">Sobre</span>
