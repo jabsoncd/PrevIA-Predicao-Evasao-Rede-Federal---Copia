c108af41b808621357fc9b9248d99c80d1c10f94
jabsoncd <jabsoncd@gmail.com>
2025-10-30 18:46:39 -0300

    self010

--- templates/Home_Profissional.py
+++ templates/Home_Profissional.py
@@ -165,11 +165,11 @@ st.markdown(f"""
 <div class="nav-container">
     <div class="nav-logo"></div>
     <div class="nav-links">
-        <a class="nav-link" href="/">Início</a>
-        <span class="nav-link" href="Simulador_Eficiencia_Layout";">Simular</span>
-        <span class="nav-link" href="Indicadores_Eficiencia_Layout';">Indicadores</span>
-        <span class="nav-link" href="#gestor">Módulo Gestor</span>
-        <span class="nav-link" href="window.location.href='#sobre';">Sobre</span>
+        <span class="nav-link" onclick="window.location.href='/';">Início</span>
+        <span class="nav-link" onclick="window.location.href="Simulador_Eficiencia_Layout";">Simular</span>
+        <span class="nav-link" onclick="window.location.href='/Indicadores_Eficiencia_Layout';">Indicadores</span>
+        <span class="nav-link" onclick="window.location.href='#gestor';">Módulo Gestor</span>
+        <span class="nav-link" onclick="window.location.href='#sobre';">Sobre</span>
     </div>
 </div>
 
