4a48d7fe8c0b5e9cfe24e8d1308e80a95c9177b2
jabsoncd <jabsoncd@gmail.com>
2025-10-31 13:20:08 -0300

    True

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -216,7 +216,7 @@ with st.container():
     st.markdown('<div class="nav-container">', unsafe_allow_html=True)
     cols = st.columns([1, 5])  # primeira col: logo, segunda: links
     with cols[0]:
-       
+        st.image("", width=140)
     with cols[1]:
         st.page_link("Home_Profisional.py", label="🏠 Início")
         st.page_link("pages/Simulador_Eficiencia_Layout.py", label="🧮 Simular")
