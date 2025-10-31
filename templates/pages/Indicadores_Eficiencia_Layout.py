b5d73c66cc0e73c8c4ed2ccc99950e7a938acd2c
jabsoncd <jabsoncd@gmail.com>
2025-10-31 12:56:30 -0300

    run_v121

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -208,11 +208,11 @@ st.markdown(f"""
 <div class="nav-container">
     <div class="nav-logo"></div>
     <div class="nav-links">
-        <span class="nav-link" onclick="window.location.href='https://previa-beta.streamlit.app/~/+/';">Início</span>
-        <span class="nav-link" onclick="window.location.href='https://previa-beta.streamlit.app/~/+/Simulador_Eficiencia_Layout';">Simular</span>
-        <span class="nav-link" onclick="window.location.href='https://previa-beta.streamlit.app/~/+/Indicadores_Eficiencia_Layout';">Indicadores</span>
-        <span class="nav-link" onclick="window.location.href='#gestor';">Módulo Gestor</span>
-        <span class="nav-link" onclick="window.location.href='#sobre';">Sobre</span>
+        <a class="nav-link" href="">Início</a>
+        <a class="nav-link" href='/Simulador_Eficiencia_Layout">Simular</a>
+        <a class="nav-link" href='/Indicadores_Eficiencia_Layout">Indicadores</a>
+        <a class="nav-link" href='#gestor';">Módulo Gestor</a>
+        <a class="nav-link" href='#sobre';">Sobre</a>
     </div>
 </div>
 
