4383ceec1545cc6b4862d0d83a480c025dc00997
jabsoncd <jabsoncd@gmail.com>
2025-10-31 12:32:43 -0300

    divisoria

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -246,22 +246,8 @@ with col2:  # Centraliza a imagem na coluna do meio
 if st.session_state.current_page == "home":
     st.markdown('<h1 class="main-header">PrevIA - Predição de Evasão na Rede Federal com Inteligência Artificial</h1>', unsafe_allow_html=True)
     st.markdown('<p class="sub-header">Sistema inteligente de análise e predição de evasão escolar na RFEPCT</p>', unsafe_allow_html=True)
-
+    st.markdown("<p style='text-align: center; color: #3f3f4f; margin-top: 0px;'>Olá! Conheça os indicadores relacionados a evasão na Rede Federal EPCT </p>", unsafe_allow_html=True)
     st.markdown("---")
-
-# 🔹 Centraliza o título
-st.markdown("<h2 style='text-align: center; color: #12125c; margin-bottom: 5px;'>Inteligência Artificial para Predição da Evasão na Rede Federal EPCT</h2>", unsafe_allow_html=True)
-
-# 🔹 Texto introdutório centralizado
-st.markdown("<p style='text-align: center; color: #1e1e8f; margin-top: 0px;'>Plataforma para análise do comportamento da evasão na RFEPCT.</p>", unsafe_allow_html=True)
-
-# 🔹 Texto introdutório centralizado
-st.markdown("<p style='text-align: center; color: #3f3f4f; margin-top: 0px;'>Olá! Conheça os indicadores relacionados a evasão na Rede Federal EPCT </p>", unsafe_allow_html=True)
-
-
-
-
-
 
 # Linha divisória
 st.write("---")
