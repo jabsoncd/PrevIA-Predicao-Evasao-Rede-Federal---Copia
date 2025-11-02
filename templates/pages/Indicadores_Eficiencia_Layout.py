90ebd6768cb5dccd2893e7b45f94dbfbd3092a09
jabsoncd <jabsoncd@gmail.com>
2025-11-02 15:18:04 -0300

    Mantem141

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -104,11 +104,7 @@ st.markdown("""
 }}
 
 /* Mantém main sem margin-left para não ser empurrado */
-main.block-container {{
-    margin-left: 0 !important;
-    transition: margin-left 0.35s ease;
-    padding-top: 3.5rem; /* espaço para nav fixa */
-}}
+
 
 /* Nav fixa no topo (exemplo) */
 .navbar {{
