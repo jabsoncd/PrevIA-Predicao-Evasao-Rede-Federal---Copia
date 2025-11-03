82158773ada4c5d2f1a1b64290494f5bbb025ada
jabsoncd <jabsoncd@gmail.com>
2025-11-03 18:57:19 -0300

    Xfeito

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -93,7 +93,6 @@ st.markdown("""
     <style>
     /* Ajusta o conteúdo da página para não ficar sobre o sidebar */
     [data-testid="stAppViewContainer"] {
-        margin-left: calc(17rem + 2cm); 
         margin-top: 0;                 /* desloca conteúdo para ficar abaixo da navbar */
     }
             
