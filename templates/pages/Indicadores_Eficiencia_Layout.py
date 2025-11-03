ef9a7358444bfb6d056d07c2e0ecdef757eb330c
jabsoncd <jabsoncd@gmail.com>
2025-11-03 16:54:41 -0300

    Retirado_ex

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -466,7 +466,7 @@ st.write(" ")
 
 ##################################################################### MODAL
 
-with st.expander("Filtros", expanded=False):
+with st.expander("Filtros"):
     st.markdown("### Situação das Matrículas")
     CATEGORIA_SITUACAO = st.multiselect(
         key="cat_situacao",
