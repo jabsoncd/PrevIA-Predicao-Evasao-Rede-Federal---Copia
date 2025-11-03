fbbf4307a7d7053053c011a5afce498813370774
jabsoncd <jabsoncd@gmail.com>
2025-11-03 16:45:29 -0300

    testerRerun

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -572,10 +572,10 @@ with st.expander("Filtros", expanded=st.session_state.expander_open):
         )
 
         # Botão "Filtrar" dentro do expander
-        if st.form_submit_button("Filtrar"):
+if st.form_submit_button("Filtrar"):
             # Fecha o expander e marca que filtros devem ser aplicados
-            st.session_state.expander_open = False
-            st.session_state.filtrar = True
+    st.session_state.expander_open = False
+    st.session_state.filtrar = True
         
 # Botão para aplicar filtros
 if st.session_state.filtrar:
