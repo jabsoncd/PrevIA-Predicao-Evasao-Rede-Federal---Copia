3d853faded3594180aab66035d39a12b26a2c3c7
jabsoncd <jabsoncd@gmail.com>
2025-11-03 19:11:34 -0300

    17remWq

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -54,7 +54,7 @@ st.markdown("""
         left: 1cm;
         top: 2cm;
         height: 100vh;
-        width: 34rem !important; 
+        width: 17rem !important; 
         min-width: 15rem !important; /* largura mínima quando recolhido */
         z-index: 9998;
         box-shadow: 2px 0 12px rgba(0,0,0,0.18);
