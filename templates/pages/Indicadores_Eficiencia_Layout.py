57302792f4cada28be567b9d2e34b047a89fa9d9
jabsoncd <jabsoncd@gmail.com>
2025-11-03 14:19:07 -0300

    colocarParent

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -60,7 +60,7 @@ st.markdown(
 
 st.markdown("""
     <style>
-    [data-testid="stSidebar"] {
+    [data-testid="stSidebar"] {{
     position: fixed !important;
     left: 0;
     top: 0;
@@ -69,7 +69,7 @@ st.markdown("""
     transition: width 0.35s ease;
     box-shadow: 2px 0 12px rgba(0,0,0,0.18);
     background-color: #f5f2c4;
-    }
+    }}
     
     /* Mantém o botão para expandir visível */
     [data-testid="collapsedControl"] {{
