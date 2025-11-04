bb4339b4315636f48375999f901aaca3a73d8492
jabsoncd <jabsoncd@gmail.com>
2025-11-04 12:39:59 -0300

    Colror_nov

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -222,7 +222,7 @@ st.markdown("""
         max-width: 20rem !important;
         z-index: 1000;
         box-shadow: 2px 0 12px rgba(0,0,0,0.18);
-        background-color: #f5f2c4;
+        background-color: #3c5382;
         transform: none !important;
         transition: none !important;
     }
