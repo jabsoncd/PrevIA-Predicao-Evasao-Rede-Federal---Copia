8b6e85649257de3747a7e4e0b1abe88ea0d171fe
jabsoncd <jabsoncd@gmail.com>
2025-11-03 23:26:45 -0300

    backgroundCdsX

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -218,9 +218,8 @@ st.markdown("""
         left: 0;
         top: 2cm;
         height: calc(100vh - 2cm);
-        width: 18rem !important;
-        min-width: 1rem !important;
-        max-width: 18rem !important;
+        min-width: 15rem !important;
+        max-width: 30rem !important;
         z-index: 1000;
         box-shadow: 2px 0 12px rgba(0,0,0,0.18);
         background-color: #f5f2c4;
