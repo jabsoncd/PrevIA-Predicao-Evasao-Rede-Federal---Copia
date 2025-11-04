09f3f80687635e1716ec4ac25ee2c91a4117efb9
jabsoncd <jabsoncd@gmail.com>
2025-11-04 16:39:14 -0300

    CentralizarB

--- templates/pages/Simulador_Eficiencia_Layout_Gestor.py
+++ templates/pages/Simulador_Eficiencia_Layout_Gestor.py
@@ -712,12 +712,13 @@ if submit:
                 show=False
             )
             
-            plt.title("Impacto das Variáveis na Predição de Evasão", fontsize=14, fontweight='bold', loc='center')
-            plt.tight_layout()
+
             
             # Centralizar o gráfico
             col1, col2, col3 = st.columns([2, 3, 2])
             with col2:
+                plt.title("Impacto das Variáveis na Predição de Evasão", fontsize=14, fontweight='bold', loc='center')
+                plt.tight_layout()
                 st.pyplot(fig)
                 plt.close()
             
