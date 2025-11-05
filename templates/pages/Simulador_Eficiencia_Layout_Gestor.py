16d71c371405d33d9185406a037b22bd3ed0a94a
jabsoncd <jabsoncd@gmail.com>
2025-11-05 18:47:04 -0300

    15Cmb

--- templates/pages/Simulador_Eficiencia_Layout_Gestor.py
+++ templates/pages/Simulador_Eficiencia_Layout_Gestor.py
@@ -131,7 +131,7 @@ st.markdown("""
     /* Sidebar com transição suave */
     [data-testid="stSidebar"] {
         position: fixed;
-        left: 12cm;
+        left: 15cm;
         top: 2cm;
         height: calc(100vh - 2cm);
         min-width: 2rem !important;
