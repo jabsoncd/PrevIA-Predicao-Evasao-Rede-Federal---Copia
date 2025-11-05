56788099f3cbed0b397ff0a35057f50297fb4e29
jabsoncd <jabsoncd@gmail.com>
2025-11-05 19:12:11 -0300

    vinte12

--- templates/pages/Simulador_Eficiencia_Layout_Gestor.py
+++ templates/pages/Simulador_Eficiencia_Layout_Gestor.py
@@ -630,7 +630,7 @@ if submit:
                 shap_values_evasao = shap_values
             
             # Criar waterfall plot
-            fig, ax = plt.subplots(figsize=(12, 10))
+            fig, ax = plt.subplots(figsize=(20, 12))
             
             # Gerar o waterfall plot
             shap.waterfall_plot(
