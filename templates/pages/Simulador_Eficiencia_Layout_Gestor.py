8003375cdbc751dc533b85623c6aebf3f58c3303
jabsoncd <jabsoncd@gmail.com>
2025-11-06 16:36:07 -0300

    write1106

--- templates/pages/Simulador_Eficiencia_Layout_Gestor.py
+++ templates/pages/Simulador_Eficiencia_Layout_Gestor.py
@@ -535,7 +535,7 @@ if st.session_state.current_page == "home":
     st.markdown('<h1 class="main-header">PrevIA - Predição de Evasão na Rede Federal com Inteligência Artificial</h1>', unsafe_allow_html=True)
     st.markdown('<p class="sub-header">Sistema inteligente de análise e predição de evasão escolar na RFEPCT</p>', unsafe_allow_html=True)
 # Linha divisória
-st.markdown("<p style='text-align: center; color: #3f3f4f; margin-top: 0px;'> </p>",
+st.markdown("<p style='text-align: center; color: #3f3f4f;'> </p>",
             unsafe_allow_html=True)
 st.write("---")
 
