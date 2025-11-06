33b9188c2eeb25169b33829333293b514d79b82e
jabsoncd <jabsoncd@gmail.com>
2025-11-06 18:21:24 -0300

    TargetSen

--- templates/pages/Sobre.py
+++ templates/pages/Sobre.py
@@ -218,11 +218,11 @@ a, a:visited, a:active {{
     <div class="nav-logo"></div>
     <div class="nav-links">
         <a class="nav-link" href="/", target="_self">Início</a>
-        <a class="nav-link" href="/Simulador_Eficiencia_Layout", target="_self">Simular</a>
-        <a class="nav-link" href="/Indicadores_Eficiencia_Layout", target="_self">Indicadores</a>
-        <a class="nav-link" href="/Simulador_Eficiencia_Layout_Gestor", target="_self">Módulo Gestor</a>
+        <a class="nav-link" href="/Simulador_Eficiencia_Layout" target="_self">Simular</a>
+        <a class="nav-link" href="/Indicadores_Eficiencia_Layout" target="_self">Indicadores</a>
+        <a class="nav-link" href="/Simulador_Eficiencia_Layout_Gestor" target="_self">Módulo Gestor</a>
         <a class="nav-link" href="/Simulador_Eficiencia_Layout_Carga" target="_self">Módulo Carga</a>
-        <a class="nav-link" href="/Sobre", target="_self">Sobre</a>
+        <a class="nav-link" href="/Sobre" target="_self">Sobre</a>
     </div>
 </div>
 
