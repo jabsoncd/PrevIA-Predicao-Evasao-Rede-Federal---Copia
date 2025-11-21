1d6b4dd3aa734e7d0b41ad5caee11e3bff58f23f
jabsoncd <jabsoncd@gmail.com>
2025-11-21 12:49:59 -0300

    zoom45

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -1521,7 +1521,7 @@ elif st.session_state.selected_tab == "🌎 Mapa da Evasão":
 
         # Criar mapa base
         mapa = folium.Map(location=[
-                        latitude_centro, longitude_centro], tiles="Cartodb Positron", zoom_start=4)
+                        latitude_centro, longitude_centro], tiles="Cartodb Positron", zoom_start=4.5)
 
         # Mapa coroplético com proporção
         Choropleth(
