6dda01524aaf7d54fbf5cffdbc253943119af091
jabsoncd <jabsoncd@gmail.com>
2025-11-21 13:09:50 -0300

    virids

--- templates/pages/Indicadores_Eficiencia_Layout.py
+++ templates/pages/Indicadores_Eficiencia_Layout.py
@@ -1529,7 +1529,7 @@ elif st.session_state.selected_tab == "🌎 Mapa da Evasão":
             data=gdf_mapa,
             columns=['NM_UF', 'proporcao'],
             key_on='properties.NM_UF',
-            fill_color='viridis', YlOrRd
+            fill_color='viridis', 
             nan_fill_color='white',
             fill_opacity=0.7,
             line_opacity=0.2
